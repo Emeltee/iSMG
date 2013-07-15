f5fdb26848c154f8d300295af781679b005ff387
Emeltee <>
2013-07-14 21:04:06 -1000

    Initial commit.

---
+++ my-gdx-game/src/com/me/mygdxgame/GameState.java
@@ -0,0 +1,24 @@
+package com.me.mygdxgame;
+
+/**
+ * Contains different possible states that games may declare themselves to be in
+ * to the application.
+ */
+public enum GameState {
+
+    /**
+     * Indicates that the game has not yet reached a win or lose state. Further
+     * updating is still required.
+     */
+    Running,
+    /**
+     * Indicates that the game has completed running, and is in the winning
+     * state. No further calls to render() are required.
+     */
+    Win,
+    /**
+     * Indicates that the game has completed running, and is in the losing
+     * state. No further calls to render() are required.
+     */
+    Lose
+}
