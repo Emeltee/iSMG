dfb6b1f3e1c87bc47a913b6c92e189e56b856138
Emeltee <Emeltee@localhost.com>
2013-08-03 17:08:18 -1000

    Fix problem with BusterShot movement/rotation. Fix EntityTestScreen not clearing. Add stub for MegaPlayer.

---
+++ my-gdx-game/src/com/me/mygdxgame/screens/seeteufelscreen/MegaPlayer.java
@@ -0,0 +1,40 @@
+package com.me.mygdxgame.screens.seeteufelscreen;
+
+import java.util.NoSuchElementException;
+
+import com.me.mygdxgame.utilities.EntityState;
+import com.me.mygdxgame.utilities.GameEntity;
+
+public class MegaPlayer implements GameEntity {
+
+    @Override
+    public void update(float deltaTime) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void draw() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public EntityState getState() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean hasCreatedEntities() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public GameEntity[] getCreatedEntities() throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
