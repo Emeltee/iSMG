a18639d9f2b1d700b9b2fcf6e1e17118600ce297
Emeltee <Emeltee@localhost.com>
2013-08-03 21:48:14 -1000

    Work on MegaPlayer. Add textures.

--- my-gdx-game/src/com/me/mygdxgame/screens/seeteufelscreen/MegaPlayer.java
+++ my-gdx-game/src/com/me/mygdxgame/screens/seeteufelscreen/MegaPlayer.java
@@ -1,36 +1,80 @@
 package com.me.mygdxgame.screens.seeteufelscreen;
 
+import java.util.ArrayDeque;
 import java.util.NoSuchElementException;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.me.mygdxgame.entities.BusterShot;
 import com.me.mygdxgame.utilities.Damageable;
 import com.me.mygdxgame.utilities.EntityState;
 import com.me.mygdxgame.utilities.GameEntity;
 
 public class MegaPlayer implements GameEntity, Damageable {
 
+    private static int MAX_HEALTH = 100;
+    private static int HITBOX_WIDTH = 26;
+    private static int HITBOX_HEIGHT = 32;
+    private static int SPRITE_WIDTH = 38;
+    private static int SPRITE_HEIGHT = 46;
+    
+    private Vector3 positon = new Vector3();
+    private Vector3 velocity = new Vector3();
+    private int health = MegaPlayer.MAX_HEALTH;
+    private Rectangle[] obstacles = null;
+    private Seeteufel seeteufel = null;
+    private Rectangle hitBox = new Rectangle(0, 0, MegaPlayer.HITBOX_WIDTH, MegaPlayer.HITBOX_HEIGHT);
+    
+    private TextureRegion[] runRight = new TextureRegion[4];
+    private TextureRegion[] runLeft = new TextureRegion[4];
+    private TextureRegion[] runShootRight = new TextureRegion[4];
+    private TextureRegion[] runShootLeft = new TextureRegion[4];
+    private TextureRegion standRight = new TextureRegion();
+    private TextureRegion standLeft = new TextureRegion();
+    private TextureRegion standShootRight = new TextureRegion();
+    private TextureRegion standShootLeft = new TextureRegion();
+    private TextureRegion[] damageRight = new TextureRegion[2];
+    private TextureRegion[] damageLeft = new TextureRegion[2];
+    private TextureRegion[] jumpLeft = new TextureRegion[2];
+    private TextureRegion[] jumpRight = new TextureRegion[2];
+    private TextureRegion[] jumpShootLeft = new TextureRegion[2];
+    private TextureRegion[] jumpShootRight = new TextureRegion[2];
+    
+    private ArrayDeque<BusterShot> newShots = new ArrayDeque<BusterShot>();
+    
+    public MegaPlayer(Texture texture, Seeteufel seeteufel, Vector3 initialPosition, Rectangle[] obstacles) {
+        this.seeteufel = seeteufel;
+        this.positon.set(initialPosition);
+    }
+    
+    public void setPosition(Vector3 position) {
+        this.positon.set(position);
+    }
+    
+    public void setObstacles(Rectangle[] obstacles) {
+        this.obstacles = obstacles;
+    }
+    
     @Override
     public void damage(int damage) {
-        // TODO Auto-generated method stub
-        
+        this.health -= damage;
     }
 
     @Override
     public int getHealth() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.health;
     }
 
     @Override
     public int getMaxHealth() {
-        // TODO Auto-generated method stub
-        return 0;
+        return MegaPlayer.MAX_HEALTH;
     }
 
     @Override
     public Rectangle[] getHitArea() {
-        // TODO
-        return new Rectangle[] {new Rectangle(1, 1, 1, 1)};
+        return new Rectangle[] {this.hitBox};
     }
 
     @Override
@@ -47,20 +91,20 @@ public class MegaPlayer implements GameEntity, Damageable {
 
     @Override
     public EntityState getState() {
-        // TODO Auto-generated method stub
+        // Always running, until the end of the screen.
         return EntityState.Running;
     }
 
     @Override
     public boolean hasCreatedEntities() {
-        // TODO Auto-generated method stub
-        return false;
+        return !this.newShots.isEmpty();
     }
 
     @Override
     public GameEntity[] getCreatedEntities() throws NoSuchElementException {
-        // TODO Auto-generated method stub
-        return null;
+        GameEntity[] returnList = (GameEntity[]) this.newShots.toArray();
+        this.newShots.clear();
+        return returnList;
     }
 
 }
