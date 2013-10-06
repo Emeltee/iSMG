4a0cc36eebf532b85812292034fa7e98685f5064
Emeltee <>
2013-10-05 18:54:35 -1000

    Add arena to top of SecondMap. Remove exit from map two. Reduce water width to just room area. Begin work on room three.

---
+++ my-gdx-game/src/com/me/mygdxgame/entities/SeeteufelSide.java
@@ -0,0 +1,54 @@
+package com.me.mygdxgame.entities;
+
+import java.util.NoSuchElementException;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.me.mygdxgame.utilities.EntityState;
+import com.me.mygdxgame.utilities.GameEntity;
+import com.me.mygdxgame.utilities.Renderer;
+
+public class SeeteufelSide implements GameEntity {
+
+    @Override
+    public void update(float deltaTime) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void draw(Renderer renderer) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public EntityState getState() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean hasCreatedEntities() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Rectangle[] getHitArea() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public GameEntity[] getCreatedEntities() throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
