51ec7695a0e71c3c036a43660617f2fbf34c9e0d
Emeltee <>
2013-10-29 18:35:04 -1000

    Adjust InfinityWaterfall grate sprite position.

--- my-gdx-game/src/com/me/mygdxgame/entities/InfinityWaterfall.java
+++ my-gdx-game/src/com/me/mygdxgame/entities/InfinityWaterfall.java
@@ -70,7 +70,7 @@ public class InfinityWaterfall implements GameEntity {
         
         // Draw grating.
         renderer.drawRegion(this.grateRegion, this.x + 1 - (this.grateRegion.getRegionWidth() - this.waterfall.getRegionWidth()) / 2.0f,
-                this.y - this.grateRegion.getRegionHeight() / 2);
+                this.y - this.grateRegion.getRegionHeight() + 2);
         
         // Animate waterfall body by shifting the texture coords.
         this.waterfall.setV(this.waterfall.getV() - InfinityWaterfall.TEX_V_DELTA);
