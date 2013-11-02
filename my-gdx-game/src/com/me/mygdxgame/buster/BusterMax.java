d06077fc1ecc1b279376afebcc70003430b5ed66
aschmidt89 <aschmid5@hawk.iit.edu>
2013-11-02 04:36:23 -0500

    Create MegaBuster class to facilitate creation of BusterShots based on stats. Add BusterPart interface to modify MegaBuster properties. Update MegaPlayer to use a MegaBuster object. Update GeminiShot to conform to new implementation.

---
+++ my-gdx-game/src/com/me/mygdxgame/buster/BusterMax.java
@@ -0,0 +1,32 @@
+package com.me.mygdxgame.buster;
+
+import com.me.mygdxgame.utilities.BusterPart;
+
+public class BusterMax implements BusterPart {
+
+    @Override
+    public String getPartName() {
+        return "Buster Max";
+    }
+
+    @Override
+    public int getAttack() {
+        return 5;
+    }
+
+    @Override
+    public int getEnergy() {
+        return 5;
+    }
+
+    @Override
+    public int getRange() {
+        return 5;
+    }
+
+    @Override
+    public int getRapid() {
+        return 5;
+    }
+
+}
---
+++ my-gdx-game/src/com/me/mygdxgame/utilities/BusterPart.java
@@ -0,0 +1,11 @@
+package com.me.mygdxgame.utilities;
+
+public interface BusterPart {
+    
+    public String getPartName();
+    public int getAttack();
+    public int getEnergy();
+    public int getRange();
+    public int getRapid();
+
+}
