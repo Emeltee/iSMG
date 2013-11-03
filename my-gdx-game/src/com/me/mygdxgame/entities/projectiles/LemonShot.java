2339b71a96d2ce4691893f75e13555c93c1a4b5c
Emeltee <>
2013-11-02 21:12:52 -1000

    Update LemonShot to take advantage of new BusterShot features. Remove unneeded code.

--- my-gdx-game/src/com/me/mygdxgame/entities/projectiles/LemonShot.java
+++ my-gdx-game/src/com/me/mygdxgame/entities/projectiles/LemonShot.java
@@ -7,50 +7,15 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector3;
 import com.me.mygdxgame.utilities.Damageable;
-import com.me.mygdxgame.utilities.EntityState;
 import com.me.mygdxgame.utilities.GameEntity;
-import com.me.mygdxgame.utilities.Renderer;
 
 public class LemonShot extends BusterShot {
-    
-    private static final int MAX_SHOTS = 6;
-    private static int currentShots = 0;
 
     public LemonShot(Texture spriteSheet, Sound missSound, Vector3 position,
             int speed, BusterShot.ShotDirection dir, int power, float range,
             Collection<GameEntity> obstacles, Collection<Damageable> targets) {
         
         super(spriteSheet, missSound, position, speed, dir, power, range, obstacles, targets);
-        
-        LemonShot.currentShots++;
-        if (LemonShot.currentShots >= LemonShot.MAX_SHOTS) {
-            this.range = 0;
-        }
-    }
-    
-    @Override
-    public void update(float deltaTime) {
-        if (this.state == EntityState.Running) {
-            super.update(deltaTime);
-            if (this.state == EntityState.Destroyed) {
-                LemonShot.currentShots--;
-            }
-        }
-    }
-    
-    @Override
-    public void draw(Renderer renderer) {
-        
-        if (this.state == EntityState.Running) {
-            renderer.drawRegion(bullet, this.position.x, this.position.y, Color.YELLOW, 1, 1, 60 * this.animationTimer);
-        }
-    }
-    
-    @Override
-    public void destroy() {
-        if (this.state == EntityState.Running) {
-            this.state = EntityState.Destroyed;
-            LemonShot.currentShots--;
-        }
+        super.setShotColor(Color.YELLOW);
     }
 }
